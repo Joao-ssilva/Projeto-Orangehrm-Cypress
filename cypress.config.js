b8c8e25c0a577c102e332155e37f6663aa6847a8
joao <joaokesleypopp@gmail.com>
2025-03-07 14:18:49 -0400

    atualizando nome das images

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,3 @@
-
-// const { defineConfig } = require("cypress")
-
-// module.exports = defineConfig({
-//   reporter: 'cypress-mochawesome-reporter',
-//   video: false,
-//   screenshotsFolder: 'cypress/reports/images',
-//   reporterOptions: {
-//     charts: true,
-//     reportPageTitle: 'Portfólio Cypress Joao Silva',
-//     embeddedScreenshots: true, 
-//     inlineAssets: true,
-//     saveAllAttempts: true
-//   },
-//   e2e: {
-//     baseUrl: 'https://opensource-demo.orangehrmlive.com',
-//     testIsolation: false,
-//     viewportWidth: 1920,
-//     viewportHeight: 1080,
-//     setupNodeEvents(on, config) {
-//       require('cypress-mochawesome-reporter/plugin')(on);
-//       return config;
-//     },
-//   },
-// })
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -33,8 +8,8 @@ module.exports = defineConfig({
     reportDir: "cypress/reports/html",
     charts: true,
     reportPageTitle: "Portfólio Cypress Joao Silva",
-    embeddedScreenshots: true, // 🔥 Ativar para incorporar prints das falhas no relatório
-    inlineAssets: true, // Permitir que os assets (imagens) sejam carregados diretamente no relatório
+    embeddedScreenshots: true,
+    inlineAssets: true,
     saveAllAttempts: false
   },
   e2e: {
